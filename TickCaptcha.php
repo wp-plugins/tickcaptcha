31ea9a87cf9ceb50504365552cf8807ce3aa2c9c
kateboomvideo <kateboomvideo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-09 03:21:48 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/tickcaptcha/trunk@1130830 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ TickCaptcha.php
@@ -0,0 +1,413 @@
+<?php
+/*
+Plugin Name: TickCaptcha
+Plugin URI: http://www.boomvideo.com.au/
+Description: Adds Video Captcha anti-spam solution to WordPress on the comment form and registration form.
+Version: 1.0.0
+Author: BOOM Technology
+Author URI: http://www.boomvideo.com.au/
+License: GNU GPL2
+*/
+global $wp_version, $tickcaptcha_vars, $tickcaptchalink, $TickCaptcha_path;
+$tickcaptchalink = "http://c.bvcau.com.au/alpha/captcha";
+$TickCaptcha_path = WP_PLUGIN_DIR.'/TickCaptcha';
+$tickcaptcha_vars = get_option('tickcaptcha_vars_db');
+register_activation_hook( plugin_basename(__FILE__), 'tickcaptcha_activate' );
+function tickcaptcha_activate()
+{
+	add_option('tickcaptcha_active', true);
+}
+function printActivateMessage()
+{
+	echo '<div id="message" class="updated"><p><strong>'._e('Settings saved.', 'tickcaptcha').'</strong></p></div>';
+}
+function tickCaptchaInit() 
+{
+	global $tickcaptcha_vars;
+
+	if(get_option('tickcaptcha_active', false)) 
+	{
+		delete_option('tickcaptcha_active');
+		add_action('admin_notices','printActivateMessage');
+		wp_redirect(admin_url("plugins.php?page=TickCaptcha&showmessage=1"));
+		exit;
+    }
+
+	add_action('admin_menu', 'tickCaptcha_add_tabs');
+	add_filter('plugin_action_links','tickCaptcha_action_links', 10, 2);
+	wp_register_style('tickcaptchaStylesheet', plugins_url('css/min_tickcaptcha_styles.css', __FILE__));
+	
+	//captcha on comment form
+	if($tickcaptcha_vars['tickcaptcha_on_comment_form'] == 'true')
+	{
+		add_action('comment_form', 'tickcaptcha_comment_form', 1);
+		add_filter('preprocess_comment', 'tickcaptcha_comment_form_check', 1);
+	}
+	
+	//captcha on registration form
+	if($tickcaptcha_vars['tickcaptcha_on_register'] == 'true')
+	{
+		add_action('signup_extra_fields', 'tickcaptcha_register_net_form');
+		add_filter('wpmu_validate_user_signup', 'tickcaptcha_register_net_form_check');
+	}
+	
+	
+	//captcha on registration form
+	if($tickcaptcha_vars['tickcaptcha_on_register'] == 'true' ) 
+	{
+		add_action('register_form', 'tickcaptcha_register_form', 1);
+		add_filter('registration_errors', 'tickcaptcha_register_form_check', 1);
+		add_action('bp_before_registration_submit_buttons', 'tickcaptcha_register_bp_form');
+		add_action('bp_signup_validate', 'tickcaptcha_register_bp_form_check');
+	}
+	
+	
+	//captcha on login form
+	if ($tickcaptcha_vars['tickcaptcha_on_login'] == 'true')
+	{	
+		add_action('login_form', 'tickcaptcha_register_form', 1);
+		add_filter('wp_authenticate_user','tickcaptcha_auth_check', 40, 3);
+	}
+	
+	
+	//captcha on forgot password form
+	if($tickcaptcha_vars['tickcaptcha_on_lostpwd'] == 'true')
+	{
+		add_action('lostpassword_form', 'tickcaptcha_register_form', 1);
+		add_filter('allow_password_reset', 'tickcaptcha_lost_password_check', 1);
+	}
+	
+	if($tickcaptcha_vars['tickcaptcha_on_cf7'] == 'true')
+	{
+		// adding Captcha to CF7
+		if (function_exists('wpcf7_add_shortcode'))
+		{
+			wpcf7_add_shortcode('tickcaptcha', 'tickcaptcha_shortcode', true );
+			add_filter( 'wpcf7_validate_tickcaptcha', 'tickcaptcha_process_custom_forms', 10, 2 );
+		}
+	}
+}
+
+/*******comment form hook starts********/
+function tickcaptcha_comment_form()
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	echo(tickcaptcha_get_html());
+	tickcaptcha_wp_move_subbmit_button();
+	return true;
+}
+
+function tickcaptcha_comment_form_check($comment)
+{		
+	global $tickcaptcha_vars;
+	if((($comment['comment_type'] != '' ) && ( $comment['comment_type'] != 'comment' )) || empty($tickcaptcha_vars['tickcaptcha_GUID']) ) {
+		return $comment;
+	}
+	if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+	{
+		wp_die('<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Please enter captcha and try again.','tickcaptcha.'));
+	}
+	if(tickcaptcha_check_result())
+	{
+		return $comment;
+	}
+	else 
+	{
+		wp_die('<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Sorry, Captcha is not correct, please try again.','tickcaptcha.'));
+	}
+}
+/*******comment form hook ends********/
+
+/*******register form hook starts********/
+function tickcaptcha_register_net_form($errors)
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	$e_captcha_wrong = $errors->get_error_message("captcha_wrong");
+	echo(tickcaptcha_get_html());
+	if( isset($e_captcha_wrong) && $e_captcha_wrong != "")
+	{
+		echo '<p class="error">' . $e_captcha_wrong . '</p>';
+	}
+	return true;
+}
+
+function tickcaptcha_register_net_form_check($errors)
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return $errors;
+	}
+	if ($_POST['stage'] == 'validate-user-signup')
+	{
+		if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+		{
+			$errors['errors']->add('captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Please enter captcha and try again.','tickcaptcha'));
+		}
+		elseif(!tickcaptcha_check_result())
+			{
+				$errors['errors']->add('captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Sorry, Captcha is not correct, please try again.','tickcaptcha'));
+			}
+	}
+	return $errors;
+}
+/*******register form hook ends********/
+
+/*******register form hook starts********/
+function tickcaptcha_register_form()
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	echo(tickcaptcha_get_html());
+	echo ('<script language="JavaScript">document.getElementById("login").style.width = "582px";</script><br>');
+	return true;
+}
+
+function tickcaptcha_register_bp_form()
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	echo(tickcaptcha_get_html());
+	return true;
+}
+
+function tickcaptcha_register_bp_form_check()
+{
+	global $bp, $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+	{
+		$bp->signup->errors['signup_username'] = __('Please enter captcha and try again.','tickcaptcha');
+	}
+	elseif(!tickcaptcha_check_result())
+		{
+			$bp->signup->errors['signup_username'] = __('Sorry, Captcha is not correct, please try again.','tickcaptcha');
+		}
+}
+
+function tickcaptcha_register_form_check($errors)
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return $errors;
+	}
+	if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+	{
+		$errors->add('captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Please enter captcha and try again.','tickcaptcha'));
+	}
+	elseif(!tickcaptcha_check_result())
+		{
+			$errors->add('captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Sorry, Captcha is not correct, please try again.','tickcaptcha'));			
+		}
+	return $errors;
+}
+
+
+function tickcaptcha_auth_check($user)
+{	
+	global $tickcaptcha_vars;
+	if((!isset( $_POST['log']))	|| empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return $user;
+	}
+	if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+	{
+		return new WP_Error( 'captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Please enter captcha and try again.','tickcaptcha') );
+	}
+	if(!tickcaptcha_check_result())
+	{
+		return new WP_Error( 'captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Sorry, Captcha is not correct, please try again.','tickcaptcha') );
+	}
+	return $user;
+}
+
+function tickcaptcha_lost_password_check($errors)
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+	{
+		return new WP_Error( 'captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Please enter captcha and try again.','tickcaptcha') );
+	}
+	if(!tickcaptcha_check_result())
+	{
+		return new WP_Error( 'captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Sorry, Captcha is not correct, please try again.','tickcaptcha') );
+	}
+	return true;
+}
+/*******register form hook ends********/
+
+/********custom contact form 7 hook*******/
+function tickcaptcha_shortcode($atts)
+{
+	global $tickcaptcha_vars;
+	if(empty($tickcaptcha_vars['tickcaptcha_GUID']))
+	{
+		return true;
+	}
+	return tickcaptcha_get_html();
+}
+
+function tickcaptcha_process_custom_forms($errors, $tag)
+{	
+	if(isset($_POST['your-message']) && $errors['valid'] ==1 )
+	{
+		if(isset($_POST['usercaptcha']) && trim($_POST['usercaptcha'])=="")
+		{
+			if ( $tag != '' ) 
+			{
+				$errors['valid'] = false;
+				$errors['reason']['tickerrorcontent'] = __('Please enter captcha and try again.','tickcaptcha');
+			} 
+			else
+			{
+				$errors['errors']->add('captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Please enter captcha and try again.','tickcaptcha'));
+			}
+		}
+		elseif (!tickcaptcha_check_result())
+			{
+				if ( $tag != '' ) 
+				{
+					$errors['valid'] = false;
+					$errors['reason']['tickerrorcontent'] = __('Sorry, Captcha is not correct, please try again.','tickcaptcha');
+				} 
+				else
+				{
+					$errors['errors']->add('captcha_wrong', '<strong>' . __('ERROR', 'tickcaptcha') . '</strong>: ' . __('Sorry, Captcha is not correct, please try again.','tickcaptcha'));
+				}
+			}
+	}
+	return $errors;
+}
+/*******custom contact form 7 hook ends********/
+
+
+function tickcaptcha_wp_move_subbmit_button()
+{
+	echo( '<div id="tickcaptcha-submit-button-area" style="margin-top:0px;" ><br /></div>
+		<script type="text/javascript">
+			var tick_subs = document.getElementsByClassName( "form-submit" );
+			var fmoved = false;
+			for(var i in tick_subs) {
+				var sub = tick_subs[i];
+				if ( sub.parentNode != undefined ) {
+					sub.parentNode.removeChild(tick_subs[i]);
+					document.getElementById("tickcaptcha-submit-button-area").appendChild(sub);
+					fmoved = true;
+				}
+			}
+			if (!fmoved) {
+				var tick_subs = document.getElementById("submit");
+				if (tick_subs!=undefined) {
+					tick_subs.parentNode.removeChild(tick_subs);
+					document.getElementById("tickcaptcha-submit-button-area").appendChild(tick_subs);
+					tick_subs.tabIndex = 6;
+				}
+			}
+		</script>' );
+}
+
+function tickcaptcha_get_html()
+{
+	global $tickcaptcha_vars, $tickcaptchalink;
+	$tickcaptcha_text = '
+						<div id="content" class="tickvideocontent">
+							 <iframe  id="boomvideoplayer" src="'.$tickcaptchalink.'/captcha_image.php?w='.$tickcaptcha_vars['tickcaptcha_width'].'&h='.$tickcaptcha_vars['tickcaptcha_height'].'&captcha='.$tickcaptcha_vars['tickcaptcha_GUID'].'&bgimg=Y" width="'.$tickcaptcha_vars['tickcaptcha_width'].'" height="'.$tickcaptcha_vars['tickcaptcha_height'].'" frameborder="0">
+							 </iframe> 
+						</div>
+						<input type="hidden" value="" id="codeid" name="codeid">
+						<input type="hidden" value="" id="usercaptcha" name="usercaptcha">
+						<script type="text/javascript" src="'.$tickcaptchalink.'/js/captchaboom_1.js"></script>
+						<span class="wpcf7-form-control-wrap boomerrorcontent">
+						</span>';
+	return $tickcaptcha_text;
+}
+
+function tickcaptcha_check_result()
+{
+	global $tickcaptchalink;
+	$url=$tickcaptchalink."/proc/valcode.php";
+	$codeid=$_POST['codeid'];
+	$usercaptcha=$_POST['usercaptcha'];
+	$data = array('codeid' => $codeid,'usercaptcha'=>$usercaptcha);
+	$options = array( 'http' => array('header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+									  'method'  => 'POST',
+									  'content' => http_build_query($data)));
+	//remove proxy from production code
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	if($result)
+	{
+		return true;
+	}
+	return false;
+}
+
+//create sub menu
+function tickCaptcha_add_tabs ()
+{		
+	add_submenu_page('plugins.php', 'Tick Captcha', 'Tick Captcha', 'manage_options', "TickCaptcha",'tickCaptcha_settings');
+	add_submenu_page('options-general.php', 'Tick Captcha Settings', 'Tick Captcha Settings', 'manage_options', "TickCaptcha",'tickCaptcha_settings');
+}
+
+//add link in menu
+function tickCaptcha_action_links($links, $file)
+{
+	static $this_plugin;
+	if (!$this_plugin)
+	{
+		$this_plugin = plugin_basename(__FILE__);
+	}
+	if ($file == $this_plugin)
+	{
+		$settings_link = '<a href="plugins.php?page=TickCaptcha">'. __('Settings', 'TickCaptcha') . '</a>';
+		array_unshift( $links, $settings_link );
+	}
+	return $links;
+}
+
+//plugin settings page call
+function tickCaptcha_settings()
+{
+	global $TickCaptcha_path;
+	wp_enqueue_style('tickcaptchaStylesheet');
+	require_once($TickCaptcha_path.'/TickCaptcha_settings.php');
+}
+
+//uninstall plugin function
+function tickCaptcha_Uninstall()
+{
+   if (basename(dirname(__FILE__)) != "mu-plugins")
+      delete_option('tickcaptcha_vars_db');
+}
+
+//initiate plugin
+add_action( 'init', 'tickCaptchaInit' );
+
+//uninstall plugin
+if(function_exists('register_uninstall_hook'))
+{
+	register_uninstall_hook(__FILE__, 'tickCaptcha_Uninstall');
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,111 @@
+=== Tick Captcha - Anti-Spam CAPTCHA ===
+Contributors: Boom Corporation Pty Ltd. 
+Tags: captcha, antispam, comment captcha, login captcha, registration captcha, contact form 7 captcha, widget, plugin, post, comments, image, images, anti-spam, admin
+Requires at least: 2.7.0
+Tested up to: 4.1
+Stable tag: /trunk/
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Our CAPTCHA is a widget that combines the sophistication of video that protects publishers from automated robot technology and provides an innovative method for publishers to monetize and brands to advertise. This takes a new turn on a mundane distorted text CAPTCHA process into a more interactive user experience whilst increasing the security of the authentication process.
+
+== Description ==
+
+Our CAPTCHA is an innovative human verification tool or CAPTCHA by BOOM Technology. TICK takes human verification to the next level by integrating video verification into the CAPTCHA process. This not only increases security and is fool proof from bots but also is a brand new monetization method for publishers and a unique medium to advertise for brands. TICK comes in the form of a clean, sleek and customizable widget that can be easily integrated into the form section on your website, blog or forum. For publishers, TICK is your solution to increased security and additional monetization. For Brands, TICK is a great new medium to advertise with increased brand recall. To know more visit the about section of this website.
+
+Our CAPTCHA protects the following forms:
+----------------------------------------
+* Registration form
+* Comment form
+* Login form
+* Lost password form
+* Contact Form 7
+
+Compatibility and Features:
+--------------------------
+
+* Our CAPTCHA supports two operating modes: Flash and HTML5. HTML5 CAPTCHA mode is used in most modern browsers, such as Mozilla FireFox, Google Chrome, Safari, and also used in iPhone and iPad. The Flash mode has been developed to be used in Internet Explorer. While loading, CAPTCHA identifies its operating mode depending on the visitor's browser.
+
+== Installation ==
+
+= Install the Plugin =
+
+There is two methods to install Tick Captcha.
+
+Method one is by downloading the plugin from here and following these steps:
+
+1. Upload TickCaptcha folder to the /wp-content/plugins/ directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Plugin settings are located in 'Plugin', 'Tick Captcha'.
+
+Method two is by downloading it directly from your Wordpress dashboard by following these steps:
+
+1. Log in to your Wordpress blog as the administrator.
+1. Navigate to Plugins -> Add New.
+1. Enter "Tick Captcha" in the search box.
+1. Click "Install Now".
+1. Once installation has completed, click the "Activate Plugin" link.
+1. Plugin settings are located in 'Plugin', 'Tick Captcha'.
+
+
+= Configure the Plugin =
+
+1. Navigate to Settings -> Tick Captcha
+1. If you have not already registered for an account, follow the instructions, to navigate to the Boom website fill in the form to become a registered Boom publisher.
+	1. Register at Boom-tick.com(http://www.boom-tick.com/register?pl=wp) for BOOM GUID/API key for plugin
+	1. Get your BOOMGUID in your email account from Tick
+	1. Enter your BOOMGUID in setting page of Tick Captcha plugin, save settings(Minimum size is 450X400)
+	1. Tick Captcha with image is ready to use.
+1. After you've registered for an account you will be assigned a BOOM GUID. Copy and paste the BOOM GUID into the Tick Captcha settings in your Wordpress admin.
+1. Select on which forms you would like Tick Captcha to be active and when you're done, click the Save Settings button.
+
+== Frequently Asked Questions ==
+
+= How can I try out your CAPTCHA? =
+Try [a demo](http://www.boom-tick.com/demo/) of Tick Captcha on our site.
+
+= WHAT IS A CAPTCHA? = 
+A CAPTCHA (an acronym for "Completely Automated Public Turing test to tell Computers and Humans Apart") is a type of challenge-response test used in computing to determine whether or not the user is human. The traditional form of CAPTCHA requires that the user type the letters of a distorted image, sometimes with the addition of an obscured sequence of letters or digits that appears on the screen.
+
+=How does Tick Captcha work? =
+Tick Captcha is a new human video verification widget by Boom Technologies. Tick Captcha uses the sophistication of video as a media to verify humans from bots. The user simply plays the video on the Tick Captcha widget, watches the video and answers a simple questions to verify that they are human.
+
+= How does the publisher make money? =
+Publishers who choose to integrate Tick Captcha into their website make money based on the advertiser video views completed on the Tick platform. There is a standard revenue share between the publisher and Boom Corporation based on the views and this is paid and reported monthly.
+
+= Does your CAPTCHA require Flash? =
+Our CAPTCHA has two modes: HTML5 and Flash. CAPTCHA loader determines the mode automatically and shows the appropriate CAPTCHA.
+
+= What browsers supports your CAPTCHA? =
+Our CAPTCHA works in all modern browsers such as Chrome, Internet Explorer, FireFox, Safari, Opera ...
+
+= Is Tick Captcha secure and safe? =
+Yes. Tick Captcha has been tested and is bot safe. We have successfully integrated Tick onto numerous leading publishers who are extremely happy with the product.
+
+= How does the integration of Tick Captcha on my site work? =
+The integrations process is very simple and quick. Our team of developers will work with the publisher to ensure this is as seamless as possible without disrupting the normal operations of the site.
+
+= How does a publisher get paid? =
+Publishers get paid monthly via Paypal or directly to their bank account.
+
+= How does a brand advertise on the tick platform? =
+Brands who are interested to leverage on the Tick Captcha advantage for advertising are encouraged to contact the Boom sales teams who will assist through the process. Please get in touch via our contact page.
+
+
+== Screenshots ==
+
+1. Admin Plugin Settings Screen
+2. Login Form
+3. Registration Form
+4. Lost Password Form
+5. Comment Form
+6. Contact Us Form
+
+== Changelog ==
+
+= 1.0.0 =
+* The first official release on WordPress.org
+* CAPTCHA for Registration form, Comment form, Login form, Lost password form, Contact Form 7
+
+== Upgrade Notice ==
+* The first official release on WordPress.org
